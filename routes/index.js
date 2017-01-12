ea467aa32b560fbf21d4e9b2013808b3b1762a4f
cunnellp5 <philip.cunnell@colorado.edu>
2017-01-11 17:14:55 -0700

    mom

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', {
-//     title: 'Phils Books',
-//     logo: '../images/book_logo.png'
-//    });
-// });
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Phils Books',
+    logo: '../images/book_logo.png'
+   });
+});
 
 module.exports = router;
