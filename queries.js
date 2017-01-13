10fed40d90c4435068e5be04ac32ddfb6f1ff8e2
cunnellp5 <philip.cunnell@colorado.edu>
2017-01-12 21:08:23 -0700

    authors list

--- queries.js
+++ queries.js
@@ -1,6 +1,10 @@
 const knex = require('./db/knex');
 
 module.exports = {
+  getAuthors: function(){
+    return knex.select().from('authors');
+  },
+
   getBooks: function(){
       // Return a promise that gets all books
       return knex.select().from('books');
@@ -17,24 +21,5 @@ module.exports = {
   getOneBook: function(id)   {
     return knex('books').where("id", id).select('*').first();
   },
-//     getBrewery: function(brewery_id){
-//         // Return a promise that gets one brewery that matches the id
-//         return knex.select().from('brewery').where('id', brewery_id);
-//     },
-//     getBeers: function(){
-//         // Return a promise that gets all beers where the beer name is set to the property beer_name
-//         // Each beer should also have a property with the name of it's brewery called brewery_name
-//         return knex.select('beer.name as beer_name', 'brewery.name as brewery_name', 'abv')
-//         .from('beer')
-//         .join('brewery', 'brewery_id', 'brewery.id');
-// },
-//     getBeersByBrewery: function(brewery_id){
-//         // Return a promise that gets all beers for a single brewery
-//         // Where the beer name is set to the property beer_name
-//         // Each beer should also have a property with the name of it's brewery called brewery_name
-//         return knex.select('beer.name as beer_name', 'brewery.name as brewery_name', 'abv')
-//         .from('beer')
-//         .join('brewery', 'brewery_id', 'brewery.id')
-//         .where('brewery.id', brewery_id);
-//     }
+
 };
--- routes/authors.js
+++ routes/authors.js
@@ -1,9 +1,14 @@
+var knex = require('../db/knex');
 var express = require('express');
 var router = express.Router();
+var queries = require('../queries');
 
-/* GET users listing. */
+/* GET authors listing. */
 router.get('/', function(req, res, next) {
-  res.send('anything');
+  queries.getAuthors().then(function(authors) {
+    res.render('authors', {authors: authors})
+  })
+
 });
 
 module.exports = router;
