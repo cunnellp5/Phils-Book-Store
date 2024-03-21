b2878d5d792dcc45f313f61e19d14db1cd1cab4a
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-21 17:56:02 -0600

    use middle wares on the service side for more routes

--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -1,50 +1,12 @@
 import { Router } from "express";
-
-import { Todo } from "../models/Todo";
+import todos from "./todos";
+import books from "./books";
+import authors from "./authors";
 
 const router = Router();
 
-router.get("/todos", async (req, res) => {
-  try {
-    const todos = await Todo.findAll();
-    res.json(todos);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-router.post("/todos", async (req, res) => {
-  try {
-    const { description } = req.body;
-    const newTodo = Todo.build({ description });
-    await newTodo.save();
-    res.sendStatus(201);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-router.delete("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const todo = await Todo.findByPk(id);
-    await todo?.destroy();
-    res.sendStatus(200);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-router.patch("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { completed, description } = req.body;
-    const todo = await Todo.findByPk(id);
-    await todo?.update({ completed, description });
-    res.sendStatus(201);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+router.use("/todos", todos);
+router.use("/books", books);
+router.use("/authors", authors);
 
 export default router;
