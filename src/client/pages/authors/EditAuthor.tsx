356bdb6a09c5cd0f22b83c28e8688c29759ff39b
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-23 20:11:52 -0600

    dummy edit pages

---
+++ src/client/pages/authors/EditAuthor.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function EditAuthor() {
+  return (
+    <div>Pass data from some store down in here then Edit this sucker</div>
+  );
+}
+
+export default EditAuthor;
---
+++ src/client/pages/books/EditBook.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function EditBook() {
+  return (
+    <div>Pass data from some store down in here then Edit this sucker</div>
+  );
+}
+
+export default EditBook;
