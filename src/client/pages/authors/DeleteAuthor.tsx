df95c58445081d89d456d9ab49b80b69ba95c835
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-23 20:11:44 -0600

    add dummy delete pages

---
+++ src/client/pages/authors/DeleteAuthor.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DeleteAuthor() {
+  return <div>Damn dude, Author deleted.. forever</div>;
+}
---
+++ src/client/pages/books/DeleteBook.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DeleteBook() {
+  return <div>Damn dude, Book deleted.. forever</div>;
+}
