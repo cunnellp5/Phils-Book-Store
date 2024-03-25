ca9940f5e539cfc61b6f0332fd34b01cb3184236
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-24 19:50:14 -0600

    add book and delete book added to client side api

--- src/client/api/books.ts
+++ src/client/api/books.ts
@@ -17,3 +17,35 @@ export async function getBook(id: number) {
 
   throw new Error("Failed to fetch book");
 }
+
+export async function addBook(data: {
+  title: string;
+  genera: string;
+  authors: string[];
+}) {
+  const res = await fetch("/api/books", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (res.ok) {
+    return await res.json();
+  }
+
+  throw new Error("Failed to add book");
+}
+
+export async function deleteBook(id: number) {
+  const res = await fetch(`/api/books/${id}`, {
+    method: "DELETE",
+  });
+
+  if (res.ok) {
+    return await res.json();
+  }
+
+  throw new Error("Failed to delete book");
+}
