bdd8a6df2af3d966ca63b570179ed10eaefda68b
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-24 18:38:51 -0600

    rm silly styles

--- src/client/styles/ItemButtons.ts
+++ src/client/styles/ItemButtons.ts
@@ -1,87 +1,22 @@
 import styled from "@emotion/styled";
 
-export const EditItemButton = styled.button`
-  background-color: #ffd700; /* gold */
-  color: #000000; /* black */
-  border: none;
-  padding: 10px 20px;
-  text-align: center;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 8px;
-  transition-duration: 0.4s;
-
-  & a {
-    text-decoration: none;
-    color: #000;
-  }
-
-  &:hover {
-    background-color: #daa520; /* goldenrod */
-    & a {
-      color: white;
-    }
-  }
-`;
-
-export const DeleteItemButton = styled.button`
-  background-color: #ff6347; /* tomato */
-  color: #ffffff; /* white */
-  border: none;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 8px;
-  transition-duration: 0.4s;
-
-  & a {
-    text-decoration: none;
-    color: #000;
-  }
-
-  &:hover {
-    background-color: #cd5c5c; /* indianred */
-    & a {
-      color: white;
-    }
-  }
-`;
-
-export const BackButton = styled.button`
-  background-color: #32cd32; /* limegreen */
-  border: none;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 8px;
-  transition-duration: 0.4s;
-
-  & a {
-    text-decoration: none;
-    color: #000;
-  }
-
-  &:hover {
-    background-color: #006400; /* darkgreen */
-    & a {
-      color: white;
-    }
-  }
-`;
-
 export const ButtonsWrapper = styled.div({
   display: "flex",
   justifyContent: "flex-end",
   width: "100%",
   gap: "10px",
+  "& button": {
+    padding: "5px 10px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+    backgroundColor: "#f9f9f9",
+    cursor: "pointer",
+    "& a": {
+      textDecoration: "none",
+      color: "#333",
+    },
+    "&:hover": {
+      backgroundColor: "#ccc0c0",
+    },
+  },
 });
