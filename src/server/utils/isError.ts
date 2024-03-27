9c3e54e1ce9b350cc33ca920f31ae395ff124406
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-27 10:28:19 -0600

    create custom error class for future use

--- src/server/utils/isError.ts
+++ src/server/utils/isError.ts
@@ -1,5 +1,14 @@
 import { Response } from "express-serve-static-core";
 
+class CustomError extends Error {
+  status: number;
+
+  constructor(message: string, status: number) {
+    super(message);
+    this.status = status;
+  }
+}
+
 export function isError(err: Error | unknown): err is Error {
   return typeof err === "object" && err !== null && "message" in err;
 }
